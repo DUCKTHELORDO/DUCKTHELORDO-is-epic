8abd5b69f56a452985a1813e500dd9aa162daf57
DUCKTHELORDO <50061315+DUCKTHELORDO@users.noreply.github.com>
2019-04-27 17:13:23 -0700

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,7 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+const token = 'NTY1MzM0NTU5MTE0Mzk1Njc2.XLy8Yw.iu4kXVVpzJLtaLv3KV2MUXXGGZI'
+
+bot.on('ready', () =>{
+    console.log('EeEeEeEeEe nice job retard, your bot is online!');
